85dd8aa071e553f1f82fe429390e69df03b545af
YaminiReddipilli <yaminireddipilli@gmail.com>
2025-05-01 15:18:15 +0530

    intial commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,7 +15,7 @@ const Hero = () => {
         </p>
         <div className='mt-8 space-x-4'>
             <a href='#contact' className='bg-gradient-to-r from-green-400 to-blue-500 text-white hidden md:inline transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full'>Contact With Me</a>
-            <a href="./MyPortfolio/YaminiReddipilli-Resume.pdf" target="_blank" className='bg-gradient-to-r from-pink-500 to-yellow-500 text-white hidden md:inline transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full'>Resume</a>
+            <a href="/MyPortfolio/YaminiReddipilli-Resume.pdf" target="_blank" className='bg-gradient-to-r from-pink-500 to-yellow-500 text-white hidden md:inline transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full'>Resume</a>
         </div>
     </div>
   )
